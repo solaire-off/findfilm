51e68b2568b5bbcb437e54d473215675c5f088a5
solairedev <work.victor.ananev@yandex.by>
2021-09-14 16:26:13 +0300

    Tested components

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Component: React.createElement
+let titleEL = React.createElement("h1", null, "Hello colleague");
+
+// Component React.Component
+class HeaderText extends React.Component {
+  render() {
+    return <p> How are you doing? </p>;
+  }
+}
+
+// Component React.PureComponent
+class BodyText extends React.PureComponent {
+  render() {
+    return <p>We are from the same city, aren't we?</p>;
+  }
+}
+
+// Functional component
+let FooterText = () => <p>Let's keep in touch</p>;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <>
+    {titleEL}
+    <HeaderText />
+    <BodyText />
+    <FooterText />
+  </>,
+  rootElement
+);
