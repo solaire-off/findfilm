0405073db0f1b0c2060712c340e6606b7503fe35
solairedev <work.victor.ananev@yandex.by>
2021-09-28 14:21:42 +0300

    CUpdated code style

--- src/components/FormControl/index.js
+++ src/components/FormControl/index.js
@@ -18,12 +18,9 @@ const FormControl = ({
     const checkFormControlStyle = STYLES.includes(formControlStyle) ? formControlStyle : '';
     const formControlClassName = `form-control ${checkFormControlStyle} ${additionalClass}`
 
-    if (formControlIsInput) {
-        return (
-            <input className={formControlClassName} type="text" placeholder={placeholder}></input>
-        )
-    }
-    return <textarea className={formControlClassName} placeholder={placeholder}>{children}</textarea>
+    return formControlIsInput
+        ? <input className={formControlClassName} type="text" placeholder={placeholder}></input>
+        : <textarea className={formControlClassName} placeholder={placeholder}>{children}</textarea>
 }
 
 FormControl.propTypes = {
