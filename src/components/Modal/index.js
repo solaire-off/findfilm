407f76ee3d91aa9e37720a36c991db0ffc9aa357
solairedev <work.victor.ananev@yandex.by>
2021-10-05 17:12:12 +0300

    Updated modal component animation

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { useDelayUnmount } from "../../Heplers"
 
 import "./Modal.sass"
 
 export const Modal = ({ isDisplay, children, additionalClass, closeCallback }) => {
+    const timeToModalFadeOut = 300;
+    const isDisplayWithDelayUnmount = useDelayUnmount(isDisplay, timeToModalFadeOut)
     const checkIsDisplay = isDisplay ? 'modal--show' : '';
     const checkAdditionalClass = additionalClass ? additionalClass : '';
 
@@ -25,7 +28,7 @@ export const Modal = ({ isDisplay, children, additionalClass, closeCallback }) =
     useEffect(() => {
         document.addEventListener('click', handleClickOutside, true)
         document.addEventListener('keydown', handleEscPress);
-        if (isDisplay) {
+        if (isDisplayWithDelayUnmount) {
             document.querySelector('html').classList.add('overflow-hidden')
         }
         return () => {
@@ -33,15 +36,18 @@ export const Modal = ({ isDisplay, children, additionalClass, closeCallback }) =
             document.removeEventListener('keydown', handleEscPress, true)
             document.querySelector('html').classList.remove('overflow-hidden')
         }
-    })
+    }, [isDisplayWithDelayUnmount, handleClickOutside, handleEscPress])
+
 
     return (
-        <div className={`modal ${checkAdditionalClass} ${checkIsDisplay}`}>
-            <div className="modal__blocker"></div>
-            <div ref={ref} className="modal__content">
-                {children}
+        isDisplayWithDelayUnmount && (
+            <div className={`modal ${checkAdditionalClass} ${checkIsDisplay}`}>
+                <div className="modal__blocker"></div>
+                <div ref={ref} className="modal__content">
+                    {children}
+                </div>
             </div>
-        </div>
+        )
     )
 }
 
